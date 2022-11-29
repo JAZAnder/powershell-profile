045d641a20c6bd063325f2964feabbc51b42cb14
Joshua Cantu <w0701691@ad.southeastern.edu>
2022-11-29 10:43:20 -0600

    Added SHit

--- RunMe.ps1
+++ RunMe.ps1
@@ -1,20 +1,84 @@
 Write-Output "Pick Which Profile You'd like to Use `n 
             
-            Option 1 `n 
-            Option 2 `n
-            Option 3 `n"
+            1 : Josh's Basic Profile`n 
+            2 : Josh's Basic Profile with Oh-My-Posh`n
+            3 : Josh's Basic Profile with Neofetch`n
+            4 : Josh's Basic Profile with Oh-My_Posh and Neofetch`n
+            5 : Downloads Terminal`n
+            10 : Exit`n"
 
 
 $Option = Read-Host -Prompt 'Enter Your Choice '
-
+$LoopBack = $true
 if ( 1 -eq $Option )
 {
     Write-Output "You Choice Option 1"
+    Copy-Item ".\Options\Microsoft.PowerShell_profile.ps1" -Destination $profile
 }
 elseif (2 -eq $Option) {
-    Write-Output "You Choice Option 2"
+    Write-Output "Pick A Following Action `n 
+            
+            1 : Use Josh's Basic Profile with Oh-My-Posh`n 
+            2 : Install Oh-My-Post`n
+            3 : Return`n
+            10 : Exit `n"
+    $Option2 = Read-Host -Prompt 'Enter Your Choice '
+    if ($Option2 -eq 1) {
+        #Copy with Oh-My-Posh
+    }
+    elseif ($Option2 -eq 2) {
+        #Downlod Oh-My-Posh
+    }
+    elseif ($Option2 -eq 10) {
+        $LoopBack = $false
+    }
 }
 elseif (3 -eq $Option) {
-    Write-Output "You Choice Option 3"
+    Write-Output "Pick A Following Action `n 
+            
+        1 : Josh's Basic Profile with Neofetch`n 
+        2 : Install Neofetch`n
+        3 : Return`n
+        10 : Exit `n"
+    $Option2 = Read-Host -Prompt 'Enter Your Choice '
+    if ($Option2 -eq 1) {
+        #Copy with Neofetch
+    }
+    elseif ($Option2 -eq 2) {
+        #Download Neofetch
+    }
+    elseif ($Option2 -eq 10) {
+        $LoopBack = $false
+    }
 }
-Pause
+elseif (4 -eq $Option) {
+    Write-Output "Pick A Following Action `n 
+            
+    1 : Josh's Basic Profile with Oh-My_Posh and Neofetch`n 
+    2 : Install Neofetch`n
+    3 : Install Oh-My-Post`n
+    4 : Return`n
+    10 : Exit `n"
+    $Option2 = Read-Host -Prompt 'Enter Your Choice '
+    if ($Option2 -eq 1) {
+        #Copy with Oh-My-Posh and Neofetch
+    }
+    elseif ($Option2 -eq 2) {
+        #Download Neofetch
+    }
+    elseif ($Option2 -eq 3) {
+        #Downlod Oh-My-Posh
+    }
+    elseif ($Option2 -eq 10) {
+        $LoopBack = $false
+    }
+}
+elseif (5 -eq $Option) {
+    Winget install Terminal
+}
+elseif ($Option -eq 10) {
+    $LoopBack = $false
+}
+if($LoopBack){
+    ./RunMe.ps1
+}
